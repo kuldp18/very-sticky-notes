a7989c211aab12a72d4641a825d1325cc24be638
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2024-07-30 22:14:52 +0530

    add tailwind and make basic header and footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,33 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <nav className="px-5 py-2 text-lg">
+        <ul className="flex gap-4 justify-between items-center">
+          <div className="left">
+            <li className="hover:text-white">
+              <Link to="/">Home</Link>
+            </li>
+          </div>
+          <div className="right flex gap-4">
+            <li className="hover:text-white">
+              <Link to="/login">Login</Link>
+            </li>
+            <li className="hover:text-white">
+              <Link to="/register">Register</Link>
+            </li>
+            <li className="hover:text-white">
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li className="hover:text-white">
+              <Link to="/profile">Logout</Link>
+            </li>
+          </div>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
